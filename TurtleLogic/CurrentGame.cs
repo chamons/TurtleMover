4ecaf4ee520cb7cff8aaaacd07a1aafbba0c27af
Chris Hamons <chris.hamons@xamarin.com>
2019-01-29 20:52:14 -0600

    First steps on turtle tag

--- TurtleLogic/CurrentGame.cs
+++ TurtleLogic/CurrentGame.cs
@@ -6,9 +6,9 @@ namespace TurtleLogic
 	public static class CurrentGame
 	{
 		// Hello World
-		//public static ITurtleGame Game = new MyTurtleHelloWorld ();
+		public static ITurtleGame Game = new MyTurtleHelloWorld ();
 
 		// Simple Game
-		public static ITurtleGame Game = new MyTurtleGame ();
+		//public static ITurtleGame Game = new MyTurtleGame ();
 	}
 }
--- TurtleLogic/MyTurtleHelloWorld.cs
+++ TurtleLogic/MyTurtleHelloWorld.cs
@@ -5,32 +5,65 @@ namespace TurtleLogic
 {
 	public class MyTurtleHelloWorld : ITurtleGame
 	{
-		Point Position = new Point (350, 200);
+		Point TurtlePosition = new Point (350, 200);
+		Point SquarePosition = new Point (75, 75);
+
+		Random random = new Random ();
+
 		public void OnDraw (long frame, ITurtleDrawing drawing)
 		{
-			drawing.Fill (new Color (192, 0, 0));
+			// Notice when bump into square
+			// when that happens, generate new location for square
+			// Bonus: Add counter number times caught
+			Color color = Colors.Red; //new Color ((byte)(random.Next () % 256), (byte)(random.Next () % 256), (byte)(random.Next () % 256));
+			drawing.Fill (color);
 			drawing.DrawText (Colors.White, new Point (200, 200), "Hello World");
-			drawing.DrawSquare (Colors.Green, new Point (75, 75), new Size (75, 75));
-			drawing.DrawTriangle (Colors.White, new Point (250, 50), ((int)frame * 2) % 360);
-			drawing.DrawTurtle (Position);
+
+			drawing.DrawSquare (Colors.Green, SquarePosition, new Size (75, 75));
+
+			drawing.DrawTriangle (Colors.White, new Point (250, 50), ((int)frame) % 360);
+
+			drawing.DrawTurtle (TurtlePosition);
 		}
 
 		public void OnKeyboard (string key)
 		{
 			switch (key) {
 			case "Left":
-				Position = new Point (Position.X - 5, Position.Y);
+				TurtlePosition = new Point (TurtlePosition.X - 5, TurtlePosition.Y);
 				break;
 			case "Right":
-				Position = new Point (Position.X + 5, Position.Y);
+				TurtlePosition = new Point (TurtlePosition.X + 5, TurtlePosition.Y);
 				break;
 			case "Up":
-				Position = new Point (Position.X, Position.Y - 5);
+				TurtlePosition = new Point (TurtlePosition.X, TurtlePosition.Y - 5);
 				break;
 			case "Down":
-				Position = new Point (Position.X, Position.Y + 5);
+				TurtlePosition = new Point (TurtlePosition.X, TurtlePosition.Y + 5);
 				break;
 			}
+
+			CheckForCollision ();
+		}
+
+		void CheckForCollision ()
+		{
+			if (SquareContainsPoint (SquarePosition, new Size (75, 75), TurtlePosition))
+			{
+				System.Diagnostics.Debug.WriteLine ("Hit!");
+			}
+			// For each corner of the square
+			// If it is within the turtle box
+			// collision
+		}
+
+		bool SquareContainsPoint (Point square, Size size, Point p)
+		{
+			int left = square.X;
+			int right = left + size.Width;
+			int top = square.Y;
+			int bottom = top + size.Height;
+			return left <= p.X && p.X <= right && top <= p.Y && p.Y <= bottom;
 		}
 
 		public void OnClick (Point position)
