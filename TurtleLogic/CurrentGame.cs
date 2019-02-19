d054695c2a1ebf62751e77c445c9e79ef202d81b
Chris Hamons <chris.hamons@xamarin.com>
2019-02-19 15:24:44 -0600

    Swap to pong start

--- TurtleLogic/CurrentGame.cs
+++ TurtleLogic/CurrentGame.cs
@@ -6,7 +6,7 @@ namespace TurtleLogic
 	public static class CurrentGame
 	{
 		// Hello World
-		public static ITurtleGame Game = new MyTurtleHelloWorld ();
+		public static ITurtleGame Game = new PongGame ();
 
 		// Simple Game
 		//public static ITurtleGame Game = new MyTurtleGame ();
---
+++ TurtleLogic/PongGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TurtleInterface;
+
+namespace TurtleLogic
+{
+	public class PongGame : ITurtleGame
+	{
+		public PongGame ()
+		{
+		}
+
+		public void OnDraw (long frame, ITurtleDrawing drawing)
+		{
+			drawing.Fill (Colors.Black);
+		}
+
+		public void OnKeyboard (string key)
+		{
+		}
+
+		public void OnClick (Point position)
+		{
+		}
+	}
+}
