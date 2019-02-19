4df9cb4dff4d08af4c0be7de3d9f368cfbcc076b
Chris Hamons <chris.hamons@xamarin.com>
2019-02-19 16:00:18 -0600

    Add notes

--- TurtleLogic/PongGame.cs
+++ TurtleLogic/PongGame.cs
@@ -12,10 +12,66 @@ namespace TurtleLogic
 		public void OnDraw (long frame, ITurtleDrawing drawing)
 		{
 			drawing.Fill (Colors.Black);
+
+			DrawPaddle (drawing);
+			DrawBall (drawing);
+			DrawScore (drawing);
+			HandlePhysics ();
+			HandleRules ();
+		}
+
+		void DrawScore (ITurtleDrawing drawing)
+		{
+			// Variable - two score variable
+			// Position to draw
+			// Do drawing
+		}
+
+		void DrawBall (ITurtleDrawing drawing)
+		{
+			// Variable - Ball Rectangle
+			// Do drawing
+		}
+	
+		void DrawPaddle (ITurtleDrawing drawing)
+		{
+			// Varible - Paddle Rectangle x 2
+			// Do drawing
+		}
+
+		void HandlePhysics ()
+		{
+			// Variable - ball velocity
+			// Compute a new ball position
+			// Compute possible collision (top, bottom, P1, P2)
+			// Compute new velocity on collsion
+		}
+
+		void HandleRules ()
+		{
+			// Best x of y
+			// Handle Reset
+				// Ball Pos, Ball Velocity, Paddle Position
+			// Handle ball off screen
+		}
+
+		void HandleInput (string direction)
+		{
+			// The paddle for that direction moves
+			// Prevent paddles going off screen
 		}
 
 		public void OnKeyboard (string key)
 		{
+			switch (key)
+			{
+				case "w":
+				case "s":
+				case "Up":
+				case "Down":
+					HandleInput (key);
+					return;
+			}
 		}
 
 		public void OnClick (Point position)
