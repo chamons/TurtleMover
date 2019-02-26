890af284c91bbe5d04706ca9a740a7553be113bc
Chris Hamons <chris.hamons@xamarin.com>
2019-02-26 16:25:44 -0600

    Draw paddles and ball

--- TurtleLogic/PongGame.cs
+++ TurtleLogic/PongGame.cs
@@ -7,6 +7,17 @@ namespace TurtleLogic
 	{
 		int PlayerOneScore = 0;
 		int PlayerTwoScore = 0;
+		Point BallPosition;
+		Size BallSize = new Size (26, 26);
+		const int LeftPaddleX = 20;
+		int RightPaddleX;
+		const int PaddleHeight = 75;
+		const int PaddleWidth = 15;
+		int LeftPaddleY;
+		int RightPaddleY;
+		bool IsInitialized = false;
+		Size PlayArea;
+		const int PaddleSpeed = 8;
 
 		public PongGame ()
 		{
@@ -14,7 +25,23 @@ namespace TurtleLogic
 
 		public void OnDraw (long frame, ITurtleDrawing drawing)
 		{
+			if (!IsInitialized)
+			{
+				int midX = (int)(drawing.ScreenWidth * .5) - (BallSize.Width / 2);
+				int midY = (int)(drawing.ScreenHeight * .5) - (BallSize.Height / 2);
+
+				BallPosition = new Point (midX, midY);
+				PlayArea = new Size ((int)drawing.ScreenWidth, (int)drawing.ScreenHeight);
+				RightPaddleX = (int)(drawing.ScreenWidth - 40);
+
+				LeftPaddleY = (int)((drawing.ScreenHeight * .5) - (PaddleHeight * .5));
+				RightPaddleY = LeftPaddleY;
+
+				IsInitialized = true;
+			}
+
 			drawing.Fill (Colors.Black);
+			drawing.DrawSquare (new Color (128, 128, 128), new Point (0, 0), PlayArea);
 
 			DrawPaddle (drawing);
 			DrawBall (drawing);
@@ -33,14 +60,13 @@ namespace TurtleLogic
 
 		void DrawBall (ITurtleDrawing drawing)
 		{
-			// Variable - Ball Rectangle
-			// Do drawing
+			drawing.DrawSquare (Colors.White, BallPosition, BallSize);
 		}
 	
 		void DrawPaddle (ITurtleDrawing drawing)
 		{
-			// Varible - Paddle Rectangle x 2
-			// Do drawing
+			drawing.DrawSquare (Colors.White, new Point (LeftPaddleX, LeftPaddleY), new Size (PaddleWidth, PaddleHeight));
+			drawing.DrawSquare (Colors.White, new Point (RightPaddleX, RightPaddleY), new Size (PaddleWidth, PaddleHeight));
 		}
 
 		void HandlePhysics ()
@@ -61,21 +87,29 @@ namespace TurtleLogic
 
 		void HandleInput (string direction)
 		{
-			// The paddle for that direction moves
-			// Prevent paddles going off screen
+			// Need to handle off screen
+			switch (direction)
+			{
+				case "w":
+					LeftPaddleY -= PaddleSpeed;
+					break;
+				case "s":
+					LeftPaddleY += PaddleSpeed;
+					break;
+				case "o":
+					RightPaddleY -= PaddleSpeed;
+					break;
+				case "l":
+					RightPaddleY += PaddleSpeed;
+					break;
+				default:
+					break;
+			}
 		}
 
 		public void OnKeyboard (string key)
 		{
-			switch (key)
-			{
-				case "w":
-				case "s":
-				case "Up":
-				case "Down":
-					HandleInput (key);
-					return;
-			}
+			HandleInput (key);
 		}
 
 		public void OnClick (Point position)
