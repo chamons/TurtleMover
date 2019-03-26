801cf6c14ec8744e54df9c814de2a3df9bbd3575
Chris Hamons <chris.hamons@xamarin.com>
2019-03-26 16:27:37 -0500

    Add pong rules

--- TurtleLogic/PongGame.cs
+++ TurtleLogic/PongGame.cs
@@ -19,6 +19,10 @@ namespace TurtleLogic
 		Size PlayArea;
 		const int PaddleSpeed = 8;
 
+		Random Random = new Random ();
+		int BallVelocityX = 0;
+		int BallVelocityY = 0;
+
 		public PongGame ()
 		{
 		}
@@ -27,15 +31,13 @@ namespace TurtleLogic
 		{
 			if (!IsInitialized)
 			{
-				int midX = (int)(drawing.ScreenWidth * .5) - (BallSize.Width / 2);
-				int midY = (int)(drawing.ScreenHeight * .5) - (BallSize.Height / 2);
-
-				BallPosition = new Point (midX, midY);
 				PlayArea = new Size ((int)drawing.ScreenWidth, (int)drawing.ScreenHeight);
 				RightPaddleX = (int)(drawing.ScreenWidth - 40);
 
 				LeftPaddleY = (int)((drawing.ScreenHeight * .5) - (PaddleHeight * .5));
 				RightPaddleY = LeftPaddleY;
+
+				SetupBall ();
 
 				IsInitialized = true;
 			}
@@ -48,6 +50,21 @@ namespace TurtleLogic
 			DrawScore (drawing);
 			HandlePhysics ();
 			HandleRules ();
+		}
+
+		void SetupBall ()
+		{
+			int midX = (int)(PlayArea.Width * .5) - (BallSize.Width / 2);
+			int midY = (int)(PlayArea.Height * .5) - (BallSize.Height / 2);
+
+			BallPosition = new Point (midX, midY);
+
+			int isLeft = Random.Next (0, 2);
+			if (isLeft == 0)
+				BallVelocityX = -5;
+			else
+				BallVelocityX = 5;
+			BallVelocityY = Random.Next (-4, 4);
 		}
 
 		void DrawScore (ITurtleDrawing drawing)
@@ -71,23 +88,115 @@ namespace TurtleLogic
 
 		void HandlePhysics ()
 		{
-			// Variable - ball velocity
-			// Compute a new ball position
-			// Compute possible collision (top, bottom, P1, P2)
-			// Compute new velocity on collsion
+			BallPosition = new Point (BallPosition.X + BallVelocityX,
+									BallPosition.Y + BallVelocityY);
+
+			if (BallPosition.Y <= 0)
+			{
+				Bounce ();
+			}
+			else if (BallPosition.Y + BallSize.Height >= PlayArea.Height)
+			{
+				Bounce ();
+			}
+
+			if (SquareContainsSquare (BallPosition, BallSize,
+				new Point (LeftPaddleX, LeftPaddleY),
+				new Size (PaddleWidth, PaddleHeight)))
+			{
+				HandleCollision ();
+			}
+			else if (SquareContainsSquare (BallPosition, BallSize,
+				new Point (RightPaddleX, RightPaddleY),
+			 	new Size (PaddleWidth, PaddleHeight)))
+			{
+				HandleCollision ();
+			}
+		}
+
+		void Bounce ()
+		{
+			BallVelocityY *= -1;
+		}
+
+		void HandleCollision ()
+		{
+			BallVelocityX *= -1;
+			BallVelocityY *= -1;
+
+			// BUG - Able to spear the ball
+		}
+
+		public void Die (bool ballWasLeft)
+		{
+			SetupBall ();
+			if (ballWasLeft)
+				PlayerTwoScore += 1;
+			else
+				PlayerOneScore += 1;
+			if (PlayerOneScore >= 11 ||
+				PlayerTwoScore >= 11 ||
+				PlayerOneScore + PlayerTwoScore >= 21)
+			{
+				PlayerOneScore = 0;
+				PlayerTwoScore = 0;
+			}
+		}
+
+		bool SquareContainsSquare (Point squareOne, Size sizeOne, Point squareTwo, Size sizeTwo)
+		{
+			Point upperLeft = squareOne;
+			Point upperRight = new Point (squareOne.X + sizeOne.Width, squareOne.Y);
+			Point lowerLeft = new Point (squareOne.X, squareOne.Y + sizeOne.Height);
+			Point lowerRight = new Point (squareOne.X + sizeOne.Width, squareOne.Y + sizeOne.Height);
+
+			if (SquareContainsPoint (squareTwo, sizeTwo, upperLeft))
+			{
+				return true;
+			}
+			else if (SquareContainsPoint (squareTwo, sizeTwo, upperRight))
+			{
+				return true;
+			}
+			else if (SquareContainsPoint (squareTwo, sizeTwo, lowerLeft))
+			{
+				return true;
+			}
+			else if (SquareContainsPoint (squareTwo, sizeTwo, lowerRight))
+			{
+				return true;
+			}
+			return false;
+		}
+
+		bool SquareContainsPoint (Point square, Size size, Point p)
+		{
+			int left = square.X;
+			int right = left + size.Width;
+			int top = square.Y;
+			int bottom = top + size.Height;
+			return left <= p.X && p.X <= right && top <= p.Y && p.Y <= bottom;
 		}
 
 		void HandleRules ()
 		{
-			// Best x of y
-			// Handle Reset
-				// Ball Pos, Ball Velocity, Paddle Position
-			// Handle ball off screen
+			if (BallPosition.X + BallSize.Width <= 0)
+			{
+				Die (true);
+			}
+			else if (BallPosition.X >= PlayArea.Width)
+			{
+				Die (false);
+			}
+			// TODO - Add ball wiggle somehow
 		}
 
 		void HandleInput (string direction)
 		{
-			// Need to handle off screen
+			// TODO - Need to handle off screen
+			// TODO - Make the paddle faster
+			// TODO - Make input smooth
+			// TODO - Add pause
 			switch (direction)
 			{
 				case "w":
